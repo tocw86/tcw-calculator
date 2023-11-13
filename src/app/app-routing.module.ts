792b39ba124598640972bf534aad3613eb053d3e
tomek <tomek@redicon.pl>
2023-11-13 17:26:33 +0100

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { HamiltonComponent } from './pages/watches/hamilton/hamilton.component';
-import { RolexSubmarinerComponent } from './pages/watches/rolex-submariner/rolex-submariner.component';
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { HamiltonComponent } from './pages/watches/hamilton/hamilton.component';
-import { RolexSubmarinerComponent } from './pages/watches/rolex-submariner/rolex-submariner.component';
-import { IconsComponent } from './components/icons/icons.component';
-
-
 
 @NgModule({
   declarations: [
