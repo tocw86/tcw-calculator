491ddee6c936a15f97a1b10098cd12a7cca55287
tomek <tomek@redicon.pl>
2023-11-13 17:47:53 +0100

    angular custom scheme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { InvoiceContractorComponent } from './components/invoice-contractor/invoice-contractor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent, HomepageComponent, InvoiceContractorComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
